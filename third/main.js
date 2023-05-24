27a817844a74f50617e2eae3cb5978e6cbf440eb
jaeheelee <jaeheelee169@gmail.com>
2023-05-24 18:09:45 +0900

    render 공통화

--- third/main.js
+++ third/main.js
@@ -9,68 +9,37 @@ var servings = [];
 var chefs = [new Chef(), new Chef()];
 var servers = [new Server(1000), new Server(2000)];
 
-function renderOders() {
-  var ordersEl = document.getElementById("orders");
-  ordersEl.innerHTML = "";
-
-  orders.forEach(function (order) {
-    var liEl = document.createElement("li");
-    liEl.textContent = order.name;
-
-    ordersEl.append(liEl);
-  });
-}
-
-function renderCookings() {
-  var cookingsEl = document.getElementById("cookings");
-  cookingsEl.innerHTML = "";
-
-  cookings.forEach(function (order) {
-    var liEl = document.createElement("li");
-    liEl.textContent = order.name;
-
-    cookingsEl.append(liEl);
-  });
-}
-
-function renderServings() {
-  var servingsEl = document.getElementById("servings");
-  servingsEl.innerHTML = "";
-
-  servings.forEach(function (order) {
-    var liEl = document.createElement("li");
-    liEl.textContent = order.name;
-
-    servingsEl.append(liEl);
-  });
-}
-
 document.getElementById("soon").onclick = function () {
   run(new Menu("순댓국", 1000));
 };
+
 document.getElementById("hae").onclick = function () {
   run(new Menu("해장국", 2000));
 };
 
-// 주문, 요리, 서빙의 메인 프로세스는 이 함수에서 전부 처리되어야 한다.
-// 화면이 뻗으면 안됨
 function run(menu) {
-  // 주문 목록에 추가, 출력
   orders.push(menu);
-  renderOders();
+  renderList({
+    id: "orders",
+    list: orders,
+  });
 
-  // 대기중인 요리사 찾기 (요리사가 있을 때까지 대기해야 함)
   findChefAsync()
     .then(function (chef) {
       orders.splice(orders.indexOf(menu), 1);
       cookings.push(menu);
 
-      renderOders();
-      renderCookings();
+      renderList({
+        id: "orders",
+        list: orders,
+      });
+
+      renderList({
+        id: "cookings",
+        list: cookings,
+      });
 
       return chef.cookAsync(menu);
-      // 요리사한테 요리 시킴
-      // -- 요리 목록으로 넘어가야 함
     })
     .then(function () {
       return findServerAsync();
@@ -79,20 +48,36 @@ function run(menu) {
       cookings.splice(cookings.indexOf(menu), 1);
       servings.push(menu);
 
-      renderCookings();
-      renderServings();
+      renderList({
+        id: "cookings",
+        list: cookings,
+      });
+      renderList({
+        id: "servings",
+        list: servings,
+      });
 
       return server.serveAsync(menu);
     })
     .then(function (menu) {
       servings.splice(servings.indexOf(menu), 1);
-      renderServings();
+      renderList({
+        id: "servings",
+        list: servings,
+      });
     });
+}
 
-  // 서빙을 시킴
-  // -- 서빙 목록으로 넘어가야 함
+function renderList(option) {
+  var listEl = document.getElementById(option.id);
+  listEl.innerHTML = "";
 
-  // 서빙 끝나면 서빙목록에서 사라짐
+  option.list.forEach(function (item) {
+    var liEl = document.createElement("li");
+    liEl.textContent = item.name;
+
+    listEl.append(liEl);
+  });
 }
 
 function findChefAsync() {
